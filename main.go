e7a9b235e463a208ea482effe02639e700e176f4
Chris Brocklesby <git@chrisbrocklesby.com>
2024-10-19 08:25:19 +1300

    6 working

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Received request")
-		w.Write([]byte("Deployed v5!"))
+		w.Write([]byte("Deployed v6! - Working"))
 	})
 
 	http.ListenAndServe(":8080", nil)
