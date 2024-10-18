8148056036d7efcbeed5df10a3d8f7842d7b097e
Chris Brocklesby <git@chrisbrocklesby.com>
2024-10-18 19:15:35 +1300

    v3

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Received request")
-		w.Write([]byte("Deployed v2"))
+		w.Write([]byte("Deployed v3"))
 	})
 
 	http.ListenAndServe(":8080", nil)
