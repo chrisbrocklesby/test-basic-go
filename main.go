0820f6b2812a790fe07f23250d1f6cebf067203d
Chris Brocklesby <git@chrisbrocklesby.com>
2024-10-27 17:33:26 +1300

    go App

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Received request")
-		w.Write([]byte("Deployed Go App - Dokploy"))
+		w.Write([]byte("Deployed Go App"))
 	})
 
 	http.ListenAndServe(":8080", nil)
